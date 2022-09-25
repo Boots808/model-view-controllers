fca41680ac40136c31006fe158bb68b95e724ad5
Raquel Stankey <brv1114@yahoo.com>
2022-09-24 21:49:23 -0500

    additional code in routes files

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,55 +1,13 @@
 const router = require("express").Router();
 const sequelize = require("../config/connection");
 const { Post, User, Comment } = require("../models");
+const withAuth = require("../utils/auth");
 
-// get all homepage posts
-router.get("/", (req, res) => {
+// get posts, ALL
+router.get("/", withAuth, (req, res) => {
   Post.findAll({
-    attributes: ["id", "post_text", "title", "created_at"],
-    include: [
-      {
-        model: Comment,
-        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
-        include: {
-          model: User,
-          attributes: ["username"],
-        },
-      },
-      {
-        model: User,
-        attributes: ["username"],
-      },
-    ],
-  })
-    .then((dbPostData) => {
-      const posts = dbPostData.map((post) => post.get({ plain: true }));
-
-      res.render("homepage", {
-        posts,
-        loggedIn: req.session.loggedIn,
-        username: req.session.username,
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
-
-router.get("/login", (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect("/");
-    return;
-  }
-
-  res.render("login");
-});
-
-//account signup
-router.get("/post/:id", (req, res) => {
-  Post.findOne({
     where: {
-      id: req.params.id,
+      user_id: req.session.user_id,
     },
     attributes: ["id", "post_text", "title", "created_at"],
     include: [
@@ -68,21 +26,52 @@ router.get("/post/:id", (req, res) => {
     ],
   })
     .then((dbPostData) => {
-      if (!dbPostData) {
-        res.status(404).json({ message: "No post found with this id" });
-        return;
-      }
-
-      const post = dbPostData.get({ plain: true });
-
-      res.render("single-post", {
-        post,
-        loggedIn: req.session.loggedIn,
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render("dashboard", {
+        posts,
+        loggedIn: true,
         username: req.session.username,
       });
     })
     .catch((err) => {
       console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+//edit code
+router.get("/edit/:id", withAuth, (req, res) => {
+  Post.findByPk(req.params.id, {
+    attributes: ["id", "post_text", "title", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (dbPostData) {
+        const post = dbPostData.get({ plain: true });
+
+        res.render("edit-post", {
+          post,
+          loggedIn: true,
+          username: req.session.username,
+        });
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((err) => {
       res.status(500).json(err);
     });
 });
